af2641fc72118b469bb7d35b5d66f743f7e1b10a
Muhammad Fajar Anshori <fajariphone2506@gmail.com>
2026-05-08 11:46:17 +0700

    Add favicon SVG icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'SOOBIN Services | Turnitin, Parafrase & Joki Tugas Termurah',
   description: 'Solusi terpercaya untuk kebutuhan akademik Anda',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
